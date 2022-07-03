ff84d5bc9a14f16a6874056593f3f565a861445b
HEE <wjdgmlckd9696@gmail.com>
2022-07-03 12:18:12 +0900

    first commit

---
+++ src/component/WeatherButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const WeatherButton = ({cities, setCity, getCurrentLocation}) => {
+    
+
+  return (
+    <div>
+        <Button variant="warning"
+                onClick={()=>getCurrentLocation()}                
+        >
+          Current Location
+        </Button>
+        
+        {cities.map((item, index)=>(
+            <Button variant="warning"
+                    key={index}
+                    onClick={()=>setCity(item)}
+            >
+                {item}
+            </Button>
+        ))};
+    </div>
+  );
+};
+
+export default WeatherButton;
